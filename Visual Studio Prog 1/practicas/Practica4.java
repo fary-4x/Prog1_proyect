8b3ee3724f419a83364b2cfe4cffd1aff64cd978
fary-4x <108238700+fary-4x@users.noreply.github.com>
2023-05-17 12:44:47 -0400

    Update Practica4.java

--- Visual Studio Prog 1/practicas/Practica4.java
+++ Visual Studio Prog 1/practicas/Practica4.java
@@ -2,9 +2,25 @@ package practicas;
 //4-Hacer un programa que permita escribir 10 nombres.
 //Y que los imprima mientras lo vayan escribiendo, una vez llegue al nombre numero 10 salga un mensaje que diga que ya no hay más cupos disponibles.
 
+import java.util.Scanner;
+
 public class Practica4 {
     
     public static void main(String[] args) {
+         String[] nombres = new String[10];
+        Scanner scanner = new Scanner(System.in);
         
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Ingresa un nombre: ");
+            String nombre = scanner.nextLine();
+            nombres[i] = nombre;
+            System.out.println(nombre);
+        }
+        
+        if (nombres.length >= 10) {
+            System.out.println("No hay más cupos disponibles");
+        }
+        
+        scanner.close();
     }
 }
